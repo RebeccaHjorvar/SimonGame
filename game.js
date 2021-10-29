b968d83d21799f874930b40d71c5e71bcd9968b3
Rebecca <rebecca.hjorvarmoscova@queenslab.se>
2021-10-29 16:50:39 +0200

    commit

---
+++ game.js
@@ -0,0 +1,16 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+const nextSequence = () => {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $(`#${randomChosenColor}`).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio('sounds/'+ randomChosenColor + '.mp3');
+  audio.play();
+};
+
+// lesson 195 - step4 jquery
+
